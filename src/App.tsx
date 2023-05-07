e119ed21172a68d369799a1c366e4c6b5116a62f
Almaz Kubakaev <almazkubakaev2607@gmail.com>
2023-05-07 20:38:45 +0600

    redux toolkit slice

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import React from "react";
-
 import "./App.css";
+import { useAppDispatch, useAppSelector } from "./hooks/redux";
+import { userSlice } from "./store/reducers/userSlice";
 
 function App() {
-  return <div className="App">Helo</div>;
+  const { count } = useAppSelector((state) => state.userReducer);
+  const { increment } = userSlice.actions;
+  const dispatch = useAppDispatch();
+
+  return (
+    <div className="App">
+      <h3>{count}</h3>
+      <button onClick={() => dispatch(increment(10))}>INCREMENT</button>
+    </div>
+  );
 }
 
 export default App;
